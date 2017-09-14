6a9ceead00ebc68ef52e0d083a334bdeeb0f4ae1
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-09-14 08:03:36 +0700

    This is a really really really long commit message that doesn't even follow the standards because I'm dumb and lazy to read the commit message guidelines

--- terrible.py
+++ terrible.py
@@ -2,16 +2,16 @@
 import sys
 import os
 import time
-name = 'Yuki'
+name='Yuki'
 class Greeter:
     def greet(self):
         print('Hello, world!')
 class NameGreeter(Greeter):
-    def __init__(self, who='world'):
-        self.__who = who
+    def __init__(self,who='world'):
+        self.__who=who
     def greet(self):
         print('Hello, {}!'.format(self.__who))
-if __name__ == '__main__':
+if __name__=='__main__':
     Greeter().greet()
     NameGreeter().greet()
     NameGreeter(name).greet()
